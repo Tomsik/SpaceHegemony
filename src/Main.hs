5c768241e337c8ef5b156a6d7a873132194ffed9
Tomasz Sikorski <sikorskit@gmail.com>
2015-02-28 01:42:12 +0100

    Separate event loop from loop step.

--- src/Main.hs
+++ src/Main.hs
@@ -7,32 +7,41 @@ import Foreign.Storable
 import Foreign.C.String
 import Foreign.Marshal.Alloc
 
-loop :: Ptr Event -> IO ()
-loop eventPtr = do
-    pollResult <- pollEvent eventPtr
-    case pollResult of
-        0 -> do
+eventLoop :: (Maybe Event -> IO Bool) -> IO ()
+eventLoop f =
+    let actualLoop eventPtr = do
+            pollResult <- pollEvent eventPtr
+            continue <- case pollResult of
+                0 -> f Nothing
+                _ -> do
+                    event <- peek eventPtr -- get event from pointer
+                    f (Just event)
+            if continue then actualLoop eventPtr else return ()
+    in alloca (\eventPtr -> actualLoop eventPtr)
+
+loopStep :: Maybe Event -> IO Bool
+loopStep maybeEvent = do
+    case maybeEvent of
+        Nothing -> do
             SDL.delay 10
-            loop eventPtr
-        _ -> do
-            event <- peek eventPtr
+            return True
+        Just event -> do
             case event of
                 (KeyboardEvent _ _ _ _ _ (Keysym scancodeQ _ _)) ->
-                    return ()
+                    return False
                 _ ->
-                    loop eventPtr
+                    return True
 
 main :: IO ()
 main = do
     i <- SDL.init initFlagEverything
     windowTitle <- newCAString "Space Hegemony"
     window <- createWindow windowTitle 0 0 800 600 0
-    alloca (\eventPtr ->
-        case i of
-            0 -> -- eveything's fine
-                loop eventPtr
-            _ -> do
-                err <- getError
-                errString <- peekCString err
-                putStrLn ("Unable to initialize SDL: " ++ errString ++ "\n"))
+    case i of
+        0 -> -- eveything's fine
+            eventLoop loopStep
+        _ -> do
+            err <- getError
+            errString <- peekCString err
+            putStrLn ("Unable to initialize SDL: " ++ errString ++ "\n")
     quit
