70bab31079f94782d9ad34cf8c69948005701321
PLkolek <mateusz.kolaczek@gmail.com>
2015-02-28 23:57:28 +0100

    Special exception type and correct message

--- src/EasierSdl.hs
+++ src/EasierSdl.hs
@@ -4,12 +4,20 @@ import Foreign.Ptr
 import Foreign.Storable
 import Foreign.Marshal.Alloc
 import Foreign.C.String
+import Foreign.C.Types
 
 import Data.Word
+import Data.Typeable
+import Control.Exception
 
 import Graphics.UI.SDL as SDL
 
 data RGB = RGB Word8 Word8 Word8
+
+data SDLException = SDLException String
+    deriving (Typeable, Show)
+
+instance Exception SDLException
 
 fillRect :: Ptr Surface -> RGB -> Rect -> IO()
 fillRect surfacePtr color rect = do
@@ -21,6 +29,7 @@ fillRect surfacePtr color rect = do
         errorCode <- SDL.fillRect surfacePtr rectPtr sdlColor
         sdlError errorCode $ return () )
 
+sdlError :: CInt -> IO a -> IO a
 sdlError errorCode success = 
     case errorCode of
         0 -> -- everything's fine
@@ -28,4 +37,4 @@ sdlError errorCode success =
         _ -> do
             err <- getError
             errString <- peekCString err
-            error $ "Unable to initialize SDL: " ++ errString ++ "\n"
+            throw . SDLException $ "Error in SDL call: " ++ errString ++ "\n"
