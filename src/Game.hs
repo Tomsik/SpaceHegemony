064e7478322235d5dc53154ae692fe7303fd1d70
PLkolek <mateusz.kolaczek@gmail.com>
2015-03-02 21:08:54 +0100

    Reorder Game.hs

--- src/Game.hs
+++ src/Game.hs
@@ -14,19 +14,18 @@ import EasierSdl
 
 type GameState = (Players, Starmap)
 
-newtype StarSystemId = StarSystemId Unique deriving (Typeable, Eq, Ord) -- all these typeable, eq and ord instances are required for ixSet
-data StarPosition = StarPosition { posx :: Integer, posy :: Integer } deriving (Typeable, Eq, Ord)
+type Starmap = IxSet StarSystem
 
+data StarPosition = StarPosition { posx :: Integer, posy :: Integer } deriving (Typeable, Eq, Ord)
+newtype StarPositionX = StarPositionX Integer deriving (Typeable, Eq, Ord) -- these two are for indexing operations only
+newtype StarPositionY = StarPositionY Integer deriving (Typeable, Eq, Ord)
+
+newtype StarSystemId = StarSystemId Unique deriving (Typeable, Eq, Ord) -- all these typeable, eq and ord instances are required for ixSet
 data StarSystem = StarSystem {
     systemId :: StarSystemId,
     position :: StarPosition,
     owner :: Maybe PlayerId
 } deriving (Typeable, Eq, Ord)
-
-newtype StarPositionX = StarPositionX Integer deriving (Typeable, Eq, Ord) -- these two are for indexing operations only
-newtype StarPositionY = StarPositionY Integer deriving (Typeable, Eq, Ord)
-
-type Starmap = IxSet StarSystem
 
 instance Indexable StarSystem where
     empty = ixSet [ -- list of indices, each ixFun returns a list of keys a starsystem has for particular index
@@ -35,13 +34,13 @@ instance Indexable StarSystem where
         ixFun (\starsystem -> [ StarPositionX . posx . position $ starsystem ]),
         ixFun (\starsystem -> [ StarPositionY . posy . position $ starsystem ]) ]
 
+own :: Player -> StarSystem -> StarSystem
+own player system = system { owner = Just . playerId $ player }
+
 makeSystem :: (Integer, Integer) -> IO StarSystem -- makeUnique returns IO Unique, hence IO
 makeSystem (x, y) = do
     id <- newUnique
     return $ StarSystem (StarSystemId id) (StarPosition x y) Nothing
-
-own :: Player -> StarSystem -> StarSystem
-own player system = system { owner = Just . playerId $ player }
 
 makeStarmap :: Players -> IO Starmap
 makeStarmap players = do
